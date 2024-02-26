3e2a71f29dba05f94fdde567cdb5302b67cea6ca
Magdalena <staramagda@gmail.com>
2024-02-26 13:46:52 +0000

    Add project files.

---
+++ RabbitReciever1/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Reciver1 App";
+
+IConnection cnn = factory.CreateConnection();
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "demo-queue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct); //adding exchange
+channel.QueueDeclare(queueName, false, false, false, null); //adding queue
+channel.QueueBind(queueName, exchangeName, routingKey, null); // binding queue to exchange
+channel.BasicQos(0, 1, false); // one message at the time
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (sender, args) =>
+{
+    Task.Delay(TimeSpan.FromSeconds(5)).Wait(); //delays reading a nex message for 5 sec
+
+    var body = args.Body.ToArray();
+
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Message received: {message}");
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTage = channel.BasicConsume(queueName, false, consumer);
+
+Console.ReadLine();
+
+channel.BasicCancel(consumerTage);
+channel.Close();
+cnn.Close();
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+factory.ClientProvidedName = "Rabbit Sender App";
+
+IConnection cnn = factory.CreateConnection();
+IModel channel = cnn.CreateModel();
+
+string exchangeName = "DemoExchange";
+string routingKey = "demo-routing-key";
+string queueName = "demo-queue";
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct); //adding exchange
+channel.QueueDeclare(queueName, false, false, false, null); //adding queue
+channel.QueueBind(queueName, exchangeName, routingKey,null); // binding queue to exchange
+
+//byte[] messageBodyBites = Encoding.UTF8.GetBytes("Hello YouTube Again"); //adding simple message
+//channel.BasicPublish(exchangeName, routingKey, null, messageBodyBites); //publishing the message
+
+for(int i = 0; i < 60; i++) 
+{
+    Console.WriteLine($"Sending Message number {i}");
+
+    byte[] messageBodyBites = Encoding.UTF8.GetBytes($"Message number {i}"); //adding simple message
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBites); //publishing the message
+    Thread.Sleep(1000); //simulating 1 sec delay 
+
+}
+
+channel.Close();
+cnn.Close();
